c1a4137cdd3f4302b4ff80cb65aeb17cfbbc993a
Joker <591713854@qq.com>
2016-12-08 15:51:11 +0800

    遮罩组件

---
+++ src/components/Mask.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-modal bacg" style="z-index:2000;" :class="{bbb:this.$store.state.show}" @click="exit"></div>
+</template>
+<script>
+  export default {
+    methods: {
+      exit(){
+        this.$store.state.show = false
+      }
+    }
+  }
+</script>
+<style>
+  .bacg {
+    display: none
+  }
+  
+  .bbb {
+    display: block
+  }
+</style>
