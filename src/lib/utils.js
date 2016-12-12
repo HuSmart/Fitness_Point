a8aec55c2e9506e95cbd5227e30cf2294e90c6bb
Joker <joker@promote.cache-dns.local>
2016-12-12 21:29:18 +0800

    优化动作列表的读取

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,5 @@
 const utils = {}
+const DEFINE_ACTION_LIST = { "胸部": [{ "name": "Deena Hopper", "desc": "Carson" }, { "name": "Bass Livingston", "desc": "Jami" }, { "name": "Marjorie Jefferson", "desc": "Holder" }, { "name": "Klein Fitzpatrick", "desc": "Munoz" }], "背部": [{ "name": "Willa Bruce", "desc": "Lois" }, { "name": "Coleman Townsend", "desc": "Diaz" }, { "name": "Adrian Andrews", "desc": "Pickett" }, { "name": "Corinne White", "desc": "Avery" }], "腿部": [{ "name": "Rodriquez Ferguson", "desc": "Frederick" }, { "name": "Aileen Holland", "desc": "Leola" }, { "name": "Margie Hamilton", "desc": "Patsy" }, { "name": "Mara Warren", "desc": "Effie" }], "肩部": [{ "name": "Rowland Simmons", "desc": "Alexander" }, { "name": "Tia Potts", "desc": "Rhea" }, { "name": "Cooke Woodward", "desc": "Alyson" }, { "name": "Faye Browning", "desc": "Melva" }], "手臂": [{ "name": "Bonnie Carney", "desc": "Lynn" }, { "name": "Kimberley Carrillo", "desc": "Leann" }, { "name": "Campbell Jimenez", "desc": "Charlene" }, { "name": "Acevedo Adams", "desc": "Beatrice" }], "腹部": [{ "name": "Genevieve Velez", "desc": "Lilly" }, { "name": "Miriam Roach", "desc": "Kathleen" }, { "name": "Benson Parsons", "desc": "Flossie" }, { "name": "Mccullough Cortez", "desc": "Koch" }], "有氧": [{ "name": "Valenzuela Hendrix", "desc": "Alberta" }, { "name": "Michael Tate", "desc": "Bryan" }, { "name": "Therese Schneider", "desc": "Santana" }, { "name": "Lesa Pratt", "desc": "Wheeler" }], "拉伸": [{ "name": "Ramona Morton", "desc": "Lourdes" }, { "name": "Saunders Warner", "desc": "Bush" }, { "name": "Augusta Maynard", "desc": "Webb" }, { "name": "Britney Mayer", "desc": "Hayes" }] }
 
 let saveToLocal = function(data){
   localStorage.setItem('recList',data)
@@ -31,13 +32,35 @@ utils.getCurrentAction = function (obj) {
   }
 }
 
-utils.syncRecToLocal = function(data) {
+utils.syncRecToLocal = function(key, data) {
   let result = JSON.stringify(data)
-  localStorage.setItem('recList',result)
+  switch (key) {
+    case 'rec':
+      localStorage.setItem('recList',result)
+      break;
+    case 'act':
+      localStorage.setItem('actionList', result)
+      break;
+    default:
+      break;
+  }
+  
 }
-utils.syncRecToApp = function(){
-  if(localStorage.getItem('recList')){
-    return JSON.parse(localStorage.getItem('recList'))
+utils.syncRecToApp = function(key){
+  switch (key) {
+    case 'rec':
+      if(localStorage.getItem('recList')){
+        return JSON.parse(localStorage.getItem('recList'))
+      }
+      break;
+    case 'act':
+      if(localStorage.getItem('actionList')){
+        return JSON.parse(localStorage.getItem('actionList'))
+      }else{
+        return DEFINE_ACTION_LIST
+      }
+    default:
+      break;
   }
   return {}
 }
--- src/store/index.js
+++ src/store/index.js
@@ -14,8 +14,9 @@ let state = {
   unit: 'Kg',
   show: false,
   selectedParams: {},
+  muscle: '',
   // 动作列表
-  actionList: { "胸部": [{ "name": "Deena Hopper", "desc": "Carson" }, { "name": "Bass Livingston", "desc": "Jami" }, { "name": "Marjorie Jefferson", "desc": "Holder" }, { "name": "Klein Fitzpatrick", "desc": "Munoz" }], "背部": [{ "name": "Willa Bruce", "desc": "Lois" }, { "name": "Coleman Townsend", "desc": "Diaz" }, { "name": "Adrian Andrews", "desc": "Pickett" }, { "name": "Corinne White", "desc": "Avery" }], "腿部": [{ "name": "Rodriquez Ferguson", "desc": "Frederick" }, { "name": "Aileen Holland", "desc": "Leola" }, { "name": "Margie Hamilton", "desc": "Patsy" }, { "name": "Mara Warren", "desc": "Effie" }], "肩部": [{ "name": "Rowland Simmons", "desc": "Alexander" }, { "name": "Tia Potts", "desc": "Rhea" }, { "name": "Cooke Woodward", "desc": "Alyson" }, { "name": "Faye Browning", "desc": "Melva" }], "手臂": [{ "name": "Bonnie Carney", "desc": "Lynn" }, { "name": "Kimberley Carrillo", "desc": "Leann" }, { "name": "Campbell Jimenez", "desc": "Charlene" }, { "name": "Acevedo Adams", "desc": "Beatrice" }], "腹部": [{ "name": "Genevieve Velez", "desc": "Lilly" }, { "name": "Miriam Roach", "desc": "Kathleen" }, { "name": "Benson Parsons", "desc": "Flossie" }, { "name": "Mccullough Cortez", "desc": "Koch" }], "有氧": [{ "name": "Valenzuela Hendrix", "desc": "Alberta" }, { "name": "Michael Tate", "desc": "Bryan" }, { "name": "Therese Schneider", "desc": "Santana" }, { "name": "Lesa Pratt", "desc": "Wheeler" }], "拉伸": [{ "name": "Ramona Morton", "desc": "Lourdes" }, { "name": "Saunders Warner", "desc": "Bush" }, { "name": "Augusta Maynard", "desc": "Webb" }, { "name": "Britney Mayer", "desc": "Hayes" }] },// 肌群列表
+  actionList: {},// 肌群列表
   muscleList: ["胸部", "背部", "手臂", "肩部", "腿部", "腹部", "有氧", "拉伸"],
   // 记录列表
   recordeList: {},
