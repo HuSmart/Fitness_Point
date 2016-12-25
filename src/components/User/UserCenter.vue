c79be6dcd7edce04f03474b3d4fffcf6c39f4af9
HuSmart <591713854@qq.com>
2016-12-25 20:34:41 +0800

    UserCenter

---
+++ src/components/User/UserCenter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="UserCenter">
+    <div class="user-center-body">
+      <div class="user-center-body">
+        <div class="user-center-top">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script></script>
+<style></style>
