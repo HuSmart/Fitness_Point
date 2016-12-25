5dbee8e43a4836750b9418e882da8800179dd46c
jsdtt <591713854@qq.com>
2016-12-25 20:28:06 +0800

    ...

--- src/components/progress/Calendar.vue
+++ src/components/progress/Calendar.vue
@@ -46,21 +46,6 @@
               header: '#3f51b5',
               headerText: '#fff'
             },
-            inputStyle: {
-              'display': 'inline-block',
-              'padding': '6px',
-              'line-height': '22px',
-              'font-size': '16px',
-              'border': '2px solid #fff',
-              'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
-              'border-radius': '2px',
-              'color': '#5F5F5F'
-            },
-            placeholder: 'when?',
-            buttons: {
-              ok: 'OK',
-              cancel: 'Cancel'
-            },
             overlayOpacity: 0.5,
             dismissible: true
           }
